1b5b0a5af8905bf1e0fdbd79f236eac145c8dc06
ABDUL SATTAR MAPARA <32198670+abdulsmapara@users.noreply.github.com>
2018-09-05 00:53:20 +0530

    Create sqrt_decomposition.cpp

---
+++ Advanced Data Structures/Sqrt Decomposition/sqrt_decomposition.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+class sqrt_decomposition
+{
+private:
+	int block_size;
+	int* blocks;
+	int* arr;
+	int length;
+public:
+	sqrt_decomposition(int* array,int len);
+	void update(int index,int new_value);
+	int query(int left,int right);
+};
+sqrt_decomposition::sqrt_decomposition(int* array,int len)
+{
+	if(array==NULL)
+		throw "NULL POINTER EXCEPTION";
+	else
+	{
+		arr=new int(len);
+		for(int i=0;i<len;i++){
+			arr[i]=array[i];
+		}
+		length=len;
+		block_size=ceil(sqrt(len));
+		blocks=new int(block_size);
+		for(int i=0;i<len;i++)
+		{
+			blocks[i/block_size] += array[i];
+		}
+	}
+}
+void sqrt_decomposition::update(int index,int new_value)
+{
+	int old_value=arr[index];
+	blocks[index/block_size] += (new_value - old_value);
+	arr[index] = new_value;
+}
+int sqrt_decomposition::query(int left,int right)
+{
+	int sum = 0;
+    while (left<right and left%block_size!=0 and left!=0)
+    {
+        sum += arr[left];
+        left++;
+    }
+    while (left+block_size <= right)
+    {
+        sum += blocks[left/block_size];
+        left += block_size;
+    }
+    while (left<=right)
+    {
+        sum += arr[left];
+        left++;
+    }
+    return sum;
+}
+signed main()
+{
+
+	//code here to use sqrt_decomposition data structure
+
+	return 0;
+}
